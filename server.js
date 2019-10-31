161c3de7e2c4312e3be3f99a9c86ce008bcf7b19
hendricendrix <hendricentrix@gmail.com>
2019-10-30 21:21:53 -0700

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('dotenv').config();
-const express = required('express');
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
